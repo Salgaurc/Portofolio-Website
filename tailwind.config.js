1874e045a2983191988427f060eb23dfe9e64b9e
Robert-Claudiu Salgau <169043725+Salgaurc@users.noreply.github.com>
2025-02-28 18:15:57 +0200

    resolved tailwind and daisyui conflicts and imports

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
-/* eslint-disable no-undef */
+import daisyui from './node_modules/daisyui'
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [
+    daisyui
+  ],
 }
+
